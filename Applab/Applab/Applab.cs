6f6b15bf74e16299800bfba15ade994511d49e3a
Junjun Guo <guojunjun@gmail.com>
2015-10-06 19:50:17 +0200

    Koble opp TodoList til forsiden;

--- Applab/Applab/Applab.cs
+++ Applab/Applab/Applab.cs
@@ -9,17 +9,7 @@ namespace Applab
 		public App ()
 		{
 			// The root page of your application
-			MainPage = new ContentPage {
-				Content = new StackLayout {
-					VerticalOptions = LayoutOptions.Center,
-					Children = {
-						new Label {
-							XAlign = TextAlignment.Center,
-							Text = "Welcome to Xamarin Forms!"
-						}
-					}
-				}
-			};
+			MainPage = new NavigationPage(new TodoList());
 		}
 
 		protected override void OnStart ()
