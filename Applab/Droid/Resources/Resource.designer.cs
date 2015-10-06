08704f101d3461cedd4a4c1d070678d65592c620
Junjun Guo <guojunjun@gmail.com>
2015-10-06 19:57:26 +0200

    Legg til ikoner for hver plattform

--- Applab/Droid/Resources/Resource.designer.cs
+++ Applab/Droid/Resources/Resource.designer.cs
@@ -45,10 +45,13 @@ namespace Applab.Droid
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int icon = 2130837504;
+			public const int ic_launcher = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int monoandroidsplash = 2130837505;
+			public const int icon = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int monoandroidsplash = 2130837506;
 			
 			static Drawable()
 			{
